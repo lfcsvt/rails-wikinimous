a01d7fb9e8708aa23a6b66e26d2b28e432040209
lucas souza <lfcsvt@gmail.com>
2016-11-22 18:51:43 +0100

    wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# read all
+  get    "articles",          to: "articles#index"
+
+  #create new task
+  get    "articles/new",      to: "articles#new"
+
+  # find a task
+  get    "articles/:id",      to: "articles#show", as: "article"
+
+
+  post   "articles",          to: "articles#create"
+
+  #edit a new task
+  get    "articles/:id/edit", to: "articles#edit", as: "editarticle"
+
+  patch  "articles/:id",      to: "articles#update"
+
+  #delete a task
+  delete "articles/:id",      to: "articles#destroy"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# TODO: Write a seed to insert 100 posts in the database
+10.times do
+  article = Article.new({
+    title: Faker::Lorem.sentence(6),
+    content: Faker::Lorem.paragraph
+  })
+  article.save!
+end
